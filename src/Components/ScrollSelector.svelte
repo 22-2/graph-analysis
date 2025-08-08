9e6069db42d4f8e933df22a65563cd5eefac23a7
22-2 <17890m@protonmail.ch>
2025-08-08 14:04:14 +0900

    ux: Algorismをselectから選択できるように

--- src/Components/ScrollSelector.svelte
+++ src/Components/ScrollSelector.svelte
@@ -1,91 +1,44 @@
 <script lang="ts">
   import type AnalysisView from 'src/AnalysisView'
-  import { ANALYSIS_TYPES, ICON } from 'src/Constants'
+  import { ANALYSIS_TYPES } from 'src/Constants'
   import type { Subtype } from 'src/Interfaces'
-  import FaGlobeAfrica from 'svelte-icons/fa/FaGlobeAfrica.svelte'
-  import IoIosChatbubbles from 'svelte-icons/io/IoIosChatbubbles.svelte'
 
   export let currSubtype: Subtype
   export let view: AnalysisView
+
+  // `bind:value` updates `currSubtype` on change.
+  // We also need to update the view's internal state.
+  function handleSelectionChange() {
+    view.currSubtype = currSubtype
+  }
 </script>
 
 {#if view.plugin.settings.algsToShow.length > 1}
-  {#key view.plugin.settings.algsToShow}
-    <div
-      class="scrollContainer"
-      on:mouseover={function () {
-        this.ariaLabel = '`Shift + Scroll` to scroll sideways'
-      }}
-    >
-      <div class="container">
-        {#each ANALYSIS_TYPES as sub}
-          {#if view.plugin.settings.algsToShow.includes(sub.subtype)}
-            <button
-              class="item GA-Button {currSubtype === sub.subtype
-                ? 'currSubtype'
-                : ''}"
-              on:click={() => {
-                currSubtype = sub.subtype
-                view.currSubtype = sub.subtype
-              }}
-            >
-              {#if sub.global}
-                <span class={ICON}>
-                  <FaGlobeAfrica />
-                </span>
-              {/if}
-              {#if sub.nlp}
-                <span class={ICON}>
-                  <IoIosChatbubbles />
-                </span>
-              {/if}
-              {sub.subtype}
-            </button>
-          {/if}
-        {/each}
-      </div>
-    </div>
-  {/key}
+  <div class="selector-container">
+    <select class="dropdown" bind:value={currSubtype} on:change={handleSelectionChange}>
+      {#each ANALYSIS_TYPES as sub}
+        {#if view.plugin.settings.algsToShow.includes(sub.subtype)}
+          <option value={sub.subtype}>
+            {#if sub.global}🌍 {/if}{#if sub.nlp}💬 {/if}{sub.subtype}
+          </option>
+        {/if}
+      {/each}
+    </select>
+  </div>
 {/if}
 
 <style>
-  .container {
-    overflow: auto;
-    white-space: nowrap;
+  .selector-container {
+    padding: 5px 10px;
   }
-
-  .GA-Button {
-    width: fit-content !important;
-    padding: 8px 5px !important;
-    margin-right: 0px;
-  }
-
-  .item {
-    display: inline-block;
-    border: 1px solid transparent;
+  .dropdown {
+    width: 100%;
+    line-height: normal;
+    padding: 0.4em;
+    padding-right: 2em;
     background-color: var(--background-primary-alt);
-    border-radius: 10px;
+    border: 1px solid var(--background-modifier-border);
+    border-radius: 5px;
     color: var(--text-normal);
-    text-align: center;
-    padding: 0px 4px;
-    margin: 0px 3px;
-    width: fit-content;
-    font-size: small;
-  }
-
-  .currSubtype {
-    font-weight: 800 !important;
-    color: var(--text-accent) !important;
-  }
-
-  .container > button:last-child {
-    margin-right: 10px;
-  }
-  .container > button:first-child {
-    margin-left: 10px;
-  }
-
-  .item:hover {
-    background-color: var(--interactive-accent);
   }
 </style>
