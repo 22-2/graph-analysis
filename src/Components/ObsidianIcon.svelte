1d5b05afe43468a8a3451a3e29edd9ffc49ce4c3
22-2 <17890m@protonmail.ch>
2026-04-14 04:52:07 +0900

    chore: Add obsidian icon

---
+++ src/Components/ObsidianIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getIcon } from "obsidian";
+
+	export let iconName: string;
+
+	function loadIcon(target: HTMLElement, iconName: string) {
+		const icon = getIcon(iconName);
+		if (icon) {
+			return target.append(icon);
+		}
+		new Error(`Icon ${iconName} not found`);
+	}
+</script>
+
+<div class="icon" use:loadIcon={iconName}></div>
